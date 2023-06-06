317a7ee159bf806f9ef485f42597386cf4c2de21
rtavared <rafael.tavares@extreme.digital>
2023-06-06 13:50:55 -0300

    Projeto Criado

---
+++ src/main/java/com/rtseven/aguas/AguasApplication.java
@@ -0,0 +1,13 @@
+package com.rtseven.aguas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtseven/aguas/AguasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtseven.aguas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AguasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
